2fdbf9c3a6aa0d5788f7c5b6365fa163f0b490cb
lemon9527 <lliu3@fellowes.com>
2025-08-08 14:10:33 +0800

    test

--- geometry.ex
+++ geometry.ex
@@ -1,36 +1,12 @@
 defmodule Geometry do
-  @moduledoc """
-  A module for basic geometric calculations.
-  """
+  defmodule Rectangle do
+    def rectangle_area(width, height), do: width * height
 
-  @doc """
-  Calculates the area of a rectangle given its width and height.
-
-  ## Examples
-
-      iex> Geometry.rectangle_area(5, 10)
-      50
-
-      iex> Geometry.rectangle_area(3, 4)
-      12
-
-  """
-  def rectangle_area(width, height) do
-    width * height
+    def square_area(side) do
+      rectangle_area(side, side)  # calls to a function in the same module
+    end
   end
 
-  @doc """
-  Calculates the area of a circle given its radius.
-
-  ## Examples
-
-      iex> Geometry.circle_area(5)
-      78.53981633974483
-
-      iex> Geometry.circle_area(3)
-      28.274333882308138
-
-  """
   def circle_area(radius) do
     :math.pi() * radius * radius
   end
